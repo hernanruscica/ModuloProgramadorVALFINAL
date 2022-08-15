55b5bf4981294f643069f84a2b591bc155de72e7
hernanruscica <cesarhernanruscica@gmail.com>
2022-08-15 10:49:51 -0300

    Creadas las tres tablas y varias inserciones

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-<h1>Cesar Hernan Ruscica</h1>
-<h1>Ejercicio de base de datos</h1>
-<h2>Consignas peluqueria canina</h2>
+<h1>ISPC - Módulo Programador - Valoración Final</h1>
+<h2>Alumno: Cesar Hernan Ruscica</h2>
+<h3>Ejercicio de base de datos</h3>
+<h3>Consignas peluqueria canina</h3>
 <a href="./peluCanina-BD-diagrama.png">diagrama BD</href>
-<h2>PASOS POR CADA ESTUDIANTE: </h2>
+<h4>PASOS POR CADA ESTUDIANTE: </h4>
 <ol>
     <li>Crear Base de Datos y Tablas según Modelo Relacional.</li>
     <li>Realizar puntos 1 y 2</li>
     <li>Elegir algunos de los puntos 3 al 12 y agregarlo al script  (diferentes entre cada uno de los integrantes)</li>
     <li>Subir Script .sql con todos los puntos mencionados a su rama personal del GitHub grupal</li>
 </ol>[
-<a href="https://acceso.ispc.edu.ar/mod/assign/view.php?id=30085">Enlace al ISPC y enunciados</a>
+<a href="https://acceso.ispc.edu.ar/mod/assign/view.php?id=30085">Enunciados</a>
 
--- peluCanina.sql
+++ peluCanina.sql
@@ -6,7 +6,7 @@ DNI int not null ,
 Nombre varchar(50) not null,
 Apellido varchar(50) not null,
 Telefono varchar(16) not null, 
-Direccion varchar(30) not null,
+Direccion varchar(50) not null,
 PRIMARY KEY (DNI)
 );
 
@@ -21,8 +21,31 @@ PRIMARY KEY (ID_Perro),
 CONSTRAINT dni_FK FOREIGN KEY (DNI_dueno) REFERENCES dueno(DNI)
 );
 
--- date type, asi se ingresa 'YYYY-MM-DD'
+CREATE TABLE  IF NOT EXISTS historial
+(
+ID_Historial int not null auto_increment,
+Fecha date not null,
+Descripcion varchar(100) not null, 
+Monto float not null,
+Perro int not null,
+PRIMARY KEY (ID_Historial),
+CONSTRAINT historial_FK FOREIGN KEY (Perro) REFERENCES perro(ID_Perro)
+);
+-- Inserciones de dueños de perros de pruebas
 INSERT INTO dueno (DNI, Nombre, Apellido, Telefono, Direccion) VALUES (28470361, 'Cesar', 'Ruscica', '1132924558', 'Mataco 1779, Libertad');
-INSERT INTO perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_dueno) VALUES (0, 'toto', '2021-12-24', 'm', 28470361);
+INSERT INTO dueno (DNI, Nombre, Apellido, Telefono, Direccion) VALUES (38471522, 'Carlos', 'Lopez', '1125458746', '9 de Julio 1549, Castelar');
+INSERT INTO dueno (DNI, Nombre, Apellido, Telefono, Direccion) VALUES (42000145, 'Maria', 'Gomez', '1112459674', 'Almafuerte 179, San Justo');
+INSERT INTO dueno (DNI, Nombre, Apellido, Telefono, Direccion) VALUES (25489765, 'Carla', 'Gonzalez', '1136854519', 'Zufriategui 854, Ituzaingo');
+INSERT INTO dueno (DNI, Nombre, Apellido, Telefono, Direccion) VALUES (25149783, 'Enrique', 'Fernandez', '1184951675', 'Av. Rivadavia 11658, San Antonio de Padua');
+-- Inserciones de perros de pruebas
+INSERT INTO perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_dueno) VALUES (null, 'toto', '2021-12-24', 'm', 28470361);
+INSERT INTO perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_dueno) VALUES (null, 'Oliver', '2021-05-20', 'm', 38471522);
+INSERT INTO perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_dueno) VALUES (null, 'kiara', '2008-03-06', 'h', 42000145);
+INSERT INTO perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_dueno) VALUES (null, 'boby', '2010-12-22', 'm', 25489765);
+INSERT INTO perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_dueno) VALUES (null, 'Firulais', '2013-11-02', 'm', 25489765);
+INSERT INTO perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_dueno) VALUES (null, 'manchita', '2010-02-15', 'h', 25149783);
+INSERT INTO perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_dueno) VALUES (null, 'chiquita', '2009-04-01', 'h', 25149783);
+-- Inserciones de historiales de pruebas
+INSERT INTO historial (ID_Historial, Fecha, Descripcion, Monto, Perro) VALUES (null, '2022-08-01', 'Lavado y baño de crema con Aloe Vera', 850.5, 2);
 
-SELECT * FROM perro;
+SELECT * FROM historial;
